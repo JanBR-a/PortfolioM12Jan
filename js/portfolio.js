5cd28a814f6f99767dcff4168c1a14ce236bcc49
JanBR-a <144946657+JanBR-a@users.noreply.github.com>
2024-02-01 18:24:57 +0100

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+var showAll = document.getElementById("Show All");
+var frontEnd = document.getElementById("FRONT-END");
+var backEnd = document.getElementById("BACK-END");
+var html5 = document.getElementById("HTML5");
+var css = document.getElementById("CSS");
+var javascript = document.getElementById("JAVASCRIPT");
+var php = document.getElementById("PHP");
+var mysql = document.getElementById("MYSQL");
+
